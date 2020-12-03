05bfa988e226242e0368ecf377cd23a3e420cf84
samia-kherbouche <kherbouchesamia28@gmail.com>
2020-12-03 10:04:05 +0100

    'init'

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import 'bootstrap/dist/css/bootstrap.min.css'
+import Todo from './Todo'
+function App() {
+  return (
+    <div className="container">
+     <Todo/>
+    </div>
+  );
+}
+
+export default App;
